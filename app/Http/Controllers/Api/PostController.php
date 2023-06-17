7554c95f0c5003f9ded853ed477d421fb53f676a
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-17 22:12:14 +0700

    Update data posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -11,6 +11,9 @@ use Illuminate\Http\Request;
 // Import Resource PostResource
 use App\Http\Resources\PostResource;
 
+// Import Facade Storage
+use Illuminate\Support\Facades\Storage;
+
 // Import Facade Validator
 use Illuminate\Support\Facades\Validator;
 
@@ -61,4 +64,45 @@ class PostController extends Controller
         // Return single post as a resource
         return new PostResource(true, 'Detail data!', $post);
     }
+
+    public function update(Request $request, $id) {
+
+        // Define validation rules
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+        ]);
+
+        // Check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        // Find post by ID
+        $post = Post::find($id);
+
+        // Check if image is not empty
+        if ($request->hasFile('image')) {
+            // Reload image
+            $image = $request->file('image');
+            $image->storeAs('public/posts', $image->hashName());
+
+            // Delete old image
+            $post->update([
+                'image' => $image->hashName(),
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        } else {
+            // Update post without image
+            $post->update([
+                'title' => $request->title,
+                'content' => $request->content
+            ]);
+        }
+        
+        // Return response
+        return new PostResource(true, 'Data berhasil diubah!', $post);
+    }
+
 }
