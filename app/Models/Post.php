51268f8111b19cd8151aadda521494d9ca2687ec
Mochammad Rafli Ramadani <keerouk.ink@gmail.com>
2023-06-17 16:14:45 +0700

    Eloquent Accessor

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -12,4 +13,10 @@ class Post extends Model
     protected $fillable = [
         'image', 'title', 'content'
     ];
+
+    protected function image(): Attribute {
+        return Attribute::make(
+            get: fn ($image) =>asset('/storage/posts/', $image),
+        );
+    }
 }
